65c58453129ce0553828c558d639807e29fbfbec
NISARG24TH <108231394+NISARG24TH@users.noreply.github.com>
2024-03-27 17:01:33 +0530

    datatype

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -10,4 +10,16 @@ let name   = "Nisarg" // string
 let age    = 5 // number
 let isLoggedIn = true // boolean
 
-//number ++
+//number ==> 2 to power 53
+// bigInt ==> 
+// string ==> ""
+// boolean ==> true / false.
+// null ==> standalone value.
+// undefined ==> 
+// symbol == unique.
+
+
+// Object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
